22689f416e4bfdb6068e73a2ea7fc4dc75abcc00
H9Solutions <paulo.artlab@gmail.com>
2022-02-23 23:04:58 -0300

    abstract base validation methods

--- src/core/class/BaseValidation.ts
+++ src/core/class/BaseValidation.ts
@@ -1,5 +1,4 @@
 export abstract class BaseValidation {
-
   readonly ZIPCODE_MASK: RegExp = /^[0-9]{5}-[0-9]{3}$/;
   readonly CNPJ_MASK: RegExp = /^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/;
   readonly TELPHONE_MASK: RegExp = /\(\d{2}\)\s\d{4,5}\-\d{4}/;
