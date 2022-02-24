2059330acc96ef98be215c4f5c38a2d49fa4a866
H9Solutions <paulo.artlab@gmail.com>
2022-02-23 22:52:14 -0300

    Base handler exception class

---
+++ src/core/class/BaseApplicationException.ts
@@ -0,0 +1,50 @@
+import {
+  CostumerValidationException,
+  InvalidExtraFieldException,
+  InvalidQueryException,
+  PrismaClientKnownRequestError,
+} from "../../modules/errors";
+import { Catch } from "../types";
+
+export abstract class BaseApplicationException{
+
+  protected InvalidExtraField(C: Catch<InvalidExtraFieldException>) {
+  
+    const { response, error } = C;
+    return response.status(error.code).json(error);
+  }
+
+  protected InvalidQuery(C: Catch<InvalidQueryException>) {
+    const { response, error } = C;
+    
+    return response.status(error.code).json(error);
+  }
+
+  protected Validation(C: Catch<CostumerValidationException>) {
+    const { response, error } = C;
+    return response.status(error.code).json(error);
+  }
+
+  protected Database(C: Catch<PrismaClientKnownRequestError>) {
+    const { response, error } = C;
+
+    if (error.code === "P2002") {
+      return response.status(400).json({
+        error: "Failed to create. Looks like you already have a register",
+      });
+    }
+    /**
+     * Failed to delete on database
+     */
+    if (error.code === "P2025") {
+      return response
+        .status(400)
+        .json({
+          error:
+            "Failed to delete, cannot match any record with provided input.",
+        });
+    } else {
+      return response.status(500).json({ error });
+    }
+  }
+}
