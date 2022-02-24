44d1a4f4aa0fb82f4f66455e5dcdf2ad8ffd920b
H9Solutions <paulo.artlab@gmail.com>
2022-02-23 23:03:28 -0300

    export exceptions

---
+++ src/core/errors/index.ts
@@ -0,0 +1,4 @@
+export { InvalidQueryException } from "./InvalidQueryException";
+export { InvalidExtraFieldException } from "./InvalidExtraFieldException";
+export { CostumerValidationException } from "./CostumerValidationException";
+export { PrismaClientKnownRequestError } from "@prisma/client/runtime";
