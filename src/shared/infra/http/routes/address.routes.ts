af06fdcd10aacef18fbb484c5186ab898d126cb1
H9Solutions <paulo.artlab@gmail.com>
2022-02-23 22:31:27 -0300

    Crud (address and costumers entities)

--- src/shared/infra/http/routes/address.routes.ts
+++ src/shared/infra/http/routes/address.routes.ts
@@ -5,9 +5,8 @@ import { addressController } from "../controllers";
 const route = Router(); 
 
 route.post("/:costumer_id", addressController.create)
-//route.put("/:id", addressController.updateById)
-//route.delete("/:id", addressController.deleteById)
-
+route.put("/:id", addressController.updateById)
+route.delete("/:costumer_id/:id", addressController.delete)
 route.get("/", addressController.findAll)
 route.get("/:id", addressController.findById)
 route.get("/costumer/:id", addressController.findByCostumerId)
