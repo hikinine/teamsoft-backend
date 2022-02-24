922ec9b84d04c980d8ba752563f576414a2a0f18
H9Solutions <paulo.artlab@gmail.com>
2022-02-23 22:53:41 -0300

    Address Dto Validation Fields

--- src/core/validation/address.validation.ts
+++ src/core/validation/address.validation.ts
@@ -1,3 +1,4 @@
+import { AddressUpdateDto } from './../../modules/dto/address.update.dto';
 import { AddressCreateDto } from "../../modules/dto";
 import {
   CostumerValidationException,
@@ -18,7 +19,35 @@ export class AddressValidation extends BaseValidation {
   ];
 
 
-  private validateAddress(address: AddressCreateDto ) {
+  update(address: AddressUpdateDto) {
+    let isValidated = false;
+
+    for (const field in address) {
+      const value = address[field];
+
+      if (!this.allowedFields?.includes(field))
+        throw new InvalidExtraFieldException(field);
+
+      if (field === "zipCode") {
+        if (!this.validateZipCode(value)) {
+          throw new CostumerValidationException(field, `Invalid zipCode Format`);
+        }
+      } else if (!this.validateNormalStringField(value)) {
+        throw new CostumerValidationException(field);
+      }
+
+      isValidated = true;
+    }
+
+    if (!isValidated)
+      throw new InvalidExtraFieldException(
+        `Address missing fields:`
+      );
+
+    return true;
+  }
+
+  create(address: AddressCreateDto) {
     let isValidated = false;
 
     let safeCopyAllowedFields = [...this.allowedFields];
@@ -31,7 +60,7 @@ export class AddressValidation extends BaseValidation {
 
       if (field === "zipCode") {
         if (!this.validateZipCode(value)) {
-          throw new CostumerValidationException(field);
+          throw new CostumerValidationException(field, `Invalid zipCode Format`);
         }
       } else if (!this.validateNormalStringField(value)) {
         throw new CostumerValidationException(field);
@@ -46,13 +75,6 @@ export class AddressValidation extends BaseValidation {
       throw new InvalidExtraFieldException(
         `Address missing fields: ${safeCopyAllowedFields?.join(", ")}`
       );
-
-    return true;
-  }
-
-  create(dto: AddressCreateDto) {
-
-    this.validateAddress(dto);
 
     return true;
   }
