f223eb339f80049fe6759befc04270872ddfea14
H9Solutions <paulo.artlab@gmail.com>
2022-02-24 21:18:55 -0300

    /v1/docs available

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,10 +2,16 @@ import { Router } from 'express';
 
 import { costumerRoute } from './costumer.routes';
 import { addressRoute } from './address.routes';
-
+import {docsHtmlString} from "../../../../docs"
 const routes = Router();
 
 routes.use('/costumer', costumerRoute);
 routes.use('/address', addressRoute);
+
+
+routes.use('/docs', (request, response) => {
+
+  return response.send(docsHtmlString)
+});
 
 export { routes };
